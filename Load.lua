d116878570e599aaa0ec4d352a7ad8a369e62669
Lucas Chua <48841053+lucaschua1@users.noreply.github.com>
2022-01-02 00:50:58 -0500

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lucaschua1/One_Punch/main/Script.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lucaschua1/One_Punch/main/Script.lua"))()
